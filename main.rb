2b1b0f4ea1e1851cc6ea6e89be6a99d78e09e774
Eko Sugeng Cahyadi <78216920+cahyadies@users.noreply.github.com>
2021-07-07 14:07:51 +0700

    update main.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,20 @@
+require_relative 'person'
 
+Jin = Person.new ("Jin Sakai", 100, 50, 0.8)
+
+
+Khotun = Person.new ("Khotun Khan", 500, 50)
+
+puts Jin
+puts Khotun
+
+while true
+    Jin.attack(Khotun)
+    break unless Khotun.isAlive?
+
+    Khotun.attack(Jin)
+    break unless Jin.isAlive?
+end
+
+puts "#{Jin.name} is dead" unless Jin.isAlive?
+puts "#{Khotun.name} is dead" unless Khotun.isAlive?
